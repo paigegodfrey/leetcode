80a6c80fabbd5833b20bb9a387e6e2b039eb0efc
Paige Godfrey <paagodfrey@gmail.com>
2020-06-25 22:29:00 -0700

    fibonacci memoization complete

--- fibonacciMemoization.js
+++ fibonacciMemoization.js
@@ -6,7 +6,6 @@ function fib(n, memo=Array(n + 1).fill(null)) {
   if (memo[n] !== null) return memo[n];
   let result = n <= 2 ? 1 : fib(n - 1, memo) + fib(n - 2, memo);
   memo[n] = result;
-  console.log(memo);
   return result;
 }
 
@@ -14,14 +13,12 @@ function fib(n, memo=Array(n + 1).fill(null)) {
 // fib(4, [x,x,x,x,x,x])                                                             + fib(3, [x,x,x,x,x,x])
 // fib(3, [x,x,x,x,x,x])                              + fib(2, [x,x,x,x,x,x])        + fib(2, [x,x,x,x,x,x]) + fib(1, [x,x,x,x,x,x])
 // fib(2, [x,x,x,x,x,x]) + fib(1, [x,x,x,x,x,x])      + 1 => memo[2] = 1;            + memo[2]               + 1 => memo[1] = 1
-// 
-// now we've memoized fib(2) and fib(1)
 //
 // memo
 // [x,x,1,x,x,x]
 // [x,1,1,x,x,x]
 // 
-// from line 16 (bottom up):
+// from line 15:
 // memo[2] = 1           + memo[1] = 1                + memo[2] = 1                  + memo[2] = 1           + memo[1] = 1
 // memo[3] = 2                                        + memo[2] = 1                  + memo[2] = 1           + memo[1] = 1
 // [x,1,1,2,x,x]
@@ -33,3 +30,15 @@ function fib(n, memo=Array(n + 1).fill(null)) {
 // [x,1,1,2,3,5]
 //
 // return 5;
+
+// BOTTOM UP APPROACH
+function fib(n) {
+  if(n <= 2) return 1;
+  let arr = [null, 1, 1];
+
+  for(let i = 3; i < n + 1; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  
+  return arr[n];
+}
