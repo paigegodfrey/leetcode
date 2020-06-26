9e75be7888f887b263a3bccc180d77837396656b
Paige Godfrey <paagodfrey@gmail.com>
2020-06-25 22:05:40 -0700

    fibonacci memoization

---
+++ fibonacciMemoization.js
@@ -0,0 +1,35 @@
+// fibonacci numbers => 1, 1, 2, 3, 5, 8, 13, ...
+// store fib(n) in Array memo at index n
+// use Array(n + 1) since arrays are zero-based
+
+function fib(n, memo=Array(n + 1).fill(null)) {
+  if (memo[n] !== null) return memo[n];
+  let result = n <= 2 ? 1 : fib(n - 1, memo) + fib(n - 2, memo);
+  memo[n] = result;
+  console.log(memo);
+  return result;
+}
+
+// fib(5);
+// fib(4, [x,x,x,x,x,x])                                                             + fib(3, [x,x,x,x,x,x])
+// fib(3, [x,x,x,x,x,x])                              + fib(2, [x,x,x,x,x,x])        + fib(2, [x,x,x,x,x,x]) + fib(1, [x,x,x,x,x,x])
+// fib(2, [x,x,x,x,x,x]) + fib(1, [x,x,x,x,x,x])      + 1 => memo[2] = 1;            + memo[2]               + 1 => memo[1] = 1
+// 
+// now we've memoized fib(2) and fib(1)
+//
+// memo
+// [x,x,1,x,x,x]
+// [x,1,1,x,x,x]
+// 
+// from line 16 (bottom up):
+// memo[2] = 1           + memo[1] = 1                + memo[2] = 1                  + memo[2] = 1           + memo[1] = 1
+// memo[3] = 2                                        + memo[2] = 1                  + memo[2] = 1           + memo[1] = 1
+// [x,1,1,2,x,x]
+// 
+// memo[4] = 3                                                                       + memo[3] = 2
+// [x,1,1,2,3,x]
+// 
+// memo[5] = 5
+// [x,1,1,2,3,5]
+//
+// return 5;
