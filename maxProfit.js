d2a51ac7d35006a7cbf0997a2c67d2ca82419e90
Paige Godfrey <paagodfrey@gmail.com>
2020-07-22 22:20:01 -0700

    maxProfit

---
+++ maxProfit.js
@@ -0,0 +1,26 @@
+// 121. Best Time to Buy and Sell Stock
+// Say you have an array for which the ith element is the price of a given stock on day i.
+//
+// If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock),
+// design an algorithm to find the maximum profit.
+//
+// Note that you cannot sell a stock before you buy one.
+
+// time O(n)
+// space O(1)
+function maxProfit (arr) {
+    let buy = 0;
+    let sell = 1;
+    let maxProfit = 0;
+    
+    while(sell < arr.length) {
+        if(arr[sell] > arr[buy]) {
+            maxProfit = Math.max(maxProfit, arr[sell] - arr[buy]);
+            sell++;
+        } else {
+            buy = sell;
+            sell++;
+        }
+    }
+    return maxProfit;
+}
--- reorderLogFiles.js
+++ reorderLogFiles.js
@@ -5,9 +5,12 @@
 //
 // Each word after the identifier will consist only of lowercase letters, or;
 // Each word after the identifier will consist only of digits.
-// We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+// We will call these two varieties of logs letter-logs and digit-logs.
+// It is guaranteed that each log has at least one word after its identifier.
 //
-// Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+// Reorder the logs so that all of the letter-logs come before any digit-log.
+// The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties. 
+// The digit-logs should be put in their original order.
 //
 // Return the final order of the logs.
 
