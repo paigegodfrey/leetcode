165f5edcdffa764fb672b282cda703c094f37bfb
Paige Godfrey <paagodfrey@gmail.com>
2020-09-24 19:55:32 -0700

    reverseList started

---
+++ reverseLinkedList.js
@@ -0,0 +1,21 @@
+// Reverse a singly linked list.
+
+// Example:
+
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
+// Follow up:
+
+// A linked list can be reversed either iteratively or recursively.
+// Could you implement both?
+
+class ListNode {
+  constructor(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+  }
+}
+
+const reverseList = list => {
+  
+}
