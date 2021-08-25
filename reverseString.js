c7d1210e89a5352dee0db67c00d0ac3b3b991e43
Paige Godfrey <paagodfrey@gmail.com>
2021-08-24 19:08:39 -0700

    reverseString final

--- reverseString.js
+++ reverseString.js
@@ -8,11 +8,25 @@
 // Example 2:
 // Input: s = ["H","a","n","n","a","h"]
 // Output: ["h","a","n","n","a","H"]
- 
+
 // Constraints:
 // 1 <= s.length <= 105
 // s[i] is a printable ascii character
 
-const reverseString = str => {
-  
+// O(n) time
+// O(1) space
+// where n = chars.length
+const reverseString = chars => {
+  if (chars.length === 1) return chars;
+
+  let left = 0;
+  let right = chars.length - 1;
+
+  while (left < right) {
+    [chars[left], chars[right]] = [chars[right], chars[left]];
+    left++;
+    right--;
+  }
+
+  return chars;
 }
